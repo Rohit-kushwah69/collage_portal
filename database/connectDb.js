337af9414f0304c5fe720935c23b42ace9d05993
Rohit3kushwah <rk6109744@gmail.com>
2025-02-10 23:41:42 -0500

    work

--- database/connectDb.js
+++ database/connectDb.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const local_url = "mongodb://127.0.0.1:27017/test01";
+const local_url = "mongodb://127.0.0.1:27017/college_portal";
 const live_URL =
   "mongodb+srv://rohitkushwah6109744:ram123@cluster0.uh5u4.mongodb.net/Test01?retryWrites=true&w=majority&appName=Cluster0";
 const connectDb = () => {
